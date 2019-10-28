e54e5d87b63208e901714cc034566015bd21449d
sato akihide <akihide315@gmail.com>
2019-10-28 18:18:42 +0900

    初めてのLARAVEL 5.6 : (5) BLADEでレイアウトを作る

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>About</title>
-</head>
-<body>
-    <!-- phpのタグ
-      <h1>About Me: <?= $first_name ?><?= $last_name ?></h1> 
-    -->
-    <h1>About Me: {{$first_name}} {{$last_name}}
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+  <h1>About Me: {{$first_name}} {{$last_name}}
+@endsection
