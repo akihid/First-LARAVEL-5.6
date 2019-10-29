d07fee3a0d730c6892eabbb0bdfef8a3485d4acd
sato akihide <akihide315@gmail.com>
2019-10-29 17:00:56 +0900

    初めてのLARAVEL 5.6 : (17) VALIDATION（入力チェック）

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Article;
 
 class ArticlesController extends Controller
@@ -16,5 +17,23 @@ class ArticlesController extends Controller
   public function show($id) {
     $article = Article::findOrFail($id);
     return view('articles.show', compact('article'));
+  }
+
+  public function create(){
+    return view('articles.create');
+  }
+
+  public function store(Request $request){
+    $rules = [
+      'title' => 'required|min:3',
+      'body' => 'required',
+      'published_at' => 'required|date',
+    ];
+
+    $validated = $this->validate($request, $rules);
+
+    Article::create($validated);
+
+    return redirect('articles');
   }
 }
